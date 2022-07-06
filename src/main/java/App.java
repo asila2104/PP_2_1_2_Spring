35cfe2a922655d5abbe4be2db8afa01edb34e59f
asila2104 <79084814+asila2104@users.noreply.github.com>
2022-07-06 12:57:40 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("For helloworld: " + bean.equals(bean2));
+        System.out.println("For cats: " + bean3.equals(bean4));
     }
 }
